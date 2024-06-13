43924f91aaeedd715603d9668be1500fd17af34f
Neven Golenic <neven.golenic@gmail.com>
2024-06-13 17:04:00 +0200

    added subroutine to check if A*A_inverted=1

--- Modules/matrix_inverse.f90
+++ Modules/matrix_inverse.f90
@@ -8,13 +8,19 @@ module matrix_inverse
 
    implicit none
 
-   public :: invert, invert_cmplx, invert_real
+   public :: invert, checkIdentity !, invert_cmplx, invert_real
    private
 
    interface invert
    !! Inverts real or complex matrix (double precision)
       procedure invert_cmplx, invert_real
    end interface invert
+
+   interface checkIdentity
+   !! Checks if real or complex matrix multiplied by its inverse 
+   !! gives an identity matrix (double precision)
+      procedure check_identity_cmplx, check_identity_real
+   end interface checkIdentity
 
 contains
   subroutine invert_cmplx(A)
@@ -129,5 +135,94 @@ contains
 
     deallocate(work)
   end subroutine invert_real
+
+subroutine check_identity_cmplx(A,A_inverted,threshold_)
+   !! Checks if a complex matrix was correctly inverted i.e. if A⁻¹ · A = 𝟙
+   !! WARNING: should be double checked see:
+   !! https://www.intel.com/content/www/us/en/docs/onemkl/developer-reference-fortran/2023-0/gemm-001.html
+   implicit none
+   complex(kind=dp), intent(in)           :: A(:,:)
+   complex(kind=dp), intent(in)           :: A_inverted(:,:)
+   real(kind=dp),    intent(in), optional :: threshold_
+
+   complex(kind=dp), allocatable :: Icheck(:,:)
+
+   integer :: Nlf
+   ! dgemm vars (inversion success check)
+   integer          :: i, K
+   real(kind=dp)    :: alpha = 1.0_dp
+   real(kind=dp)    :: beta  = 0.0_dp
+   complex(kind=dp) :: checkIdentity, checkIdentity2
+   complex(kind=dp) :: traceIdentity
+   real(kind=dp)    :: threshold = dble(10d-4) ! default value arbitrarily chosen
+
+   if (present(threshold_)) threshold = threshold_
+
+   Nlf = size(A,1)
+   K = Nlf   
+   allocate(Icheck(Nlf,Nlf))
+
+   call zgemm('N','N', Nlf, Nlf, K, alpha, A_inverted, Nlf, A, K, beta, Icheck, Nlf)
+   traceIdentity = sum( (/ ( abs(Icheck(i,i)), i=1, size(Icheck, 1)) /) )
+   checkIdentity = sum(abs(Icheck)) - traceIdentity
+   checkIdentity2 = dcmplx(Nlf,0.0_dp) - traceIdentity
+   if ( dble(checkIdentity)>threshold .or. &
+      & aimag(checkIdentity)>threshold .or. &
+      & dble(checkIdentity2)>threshold .or. &
+      & aimag(checkIdentity2)>threshold) then
+     print *, 'Nlf: ', Nlf
+     print *, 'size Ainv: ', size(A_inverted), 'size A:',size(A)
+     print *, 'Re(check): ', dble(checkIdentity)
+     print *, 'Im(check): ', aimag(checkIdentity)
+     print *, 'Re(check2): ', dble(checkIdentity2)
+     print *, 'Im(check2): ', aimag(checkIdentity2)
+     call error('Matrix inversion failed. (A⁻¹ · A ≠ 𝟙)')
+   endif
+
+   deallocate(Icheck)
+end subroutine check_identity_cmplx
+
+subroutine check_identity_real(A,A_inverted,threshold_)
+   !! Checks if a real matrix was correctly inverted i.e. if A⁻¹ · A = 𝟙
+   !! WARNING: should be double checked see:
+   !! https://www.intel.com/content/www/us/en/docs/onemkl/developer-reference-fortran/2023-0/gemm-001.html
+   implicit none
+   real(kind=dp), intent(in)           :: A(:,:)
+   real(kind=dp), intent(in)           :: A_inverted(:,:)
+   real(kind=dp), intent(in), optional :: threshold_
+
+   real(kind=dp), allocatable :: Icheck(:,:)
+
+   integer :: Nlf
+   ! dgemm vars (inversion success check)
+   integer          :: i, K
+   real(kind=dp)    :: alpha = 1.0_dp
+   real(kind=dp)    :: beta  = 0.0_dp
+   real(kind=dp)    :: checkIdentity, checkIdentity2
+   real(kind=dp)    :: traceIdentity
+   real(kind=dp)    :: threshold = dble(10d-4) ! default value arbitrarily chosen
+
+   if (present(threshold_)) threshold = threshold_
+
+   Nlf = size(A,1)
+   K = Nlf   
+   allocate(Icheck(Nlf,Nlf))
+
+   call dgemm('N','N', Nlf, Nlf, K, alpha, A_inverted, Nlf, A, K, beta, Icheck, Nlf)
+   traceIdentity = sum( (/ ( abs(Icheck(i,i)), i=1, size(Icheck, 1)) /) )
+   checkIdentity = sum(abs(Icheck)) - traceIdentity
+   checkIdentity2 = Nlf - traceIdentity
+   if ( dble(checkIdentity)>threshold .or. &
+      & dble(checkIdentity2)>threshold) then
+     print *, 'Nlf: ', Nlf
+     print *, 'size Ainv: ', size(A_inverted), 'size A:',size(A)
+     print *, 'check: ', dble(checkIdentity)
+     print *, 'check2: ', dble(checkIdentity2)
+     call error('Matrix inversion failed. (A⁻¹ · A ≠ 𝟙)')
+   endif
+
+   deallocate(Icheck)
+end subroutine check_identity_real
+
 
 end module matrix_inverse
